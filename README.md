85cbd6dd06440a041e13459395d988dddf2810fe
rishidaboo <rishi.daboo02@gmail.com>
2021-12-07 14:55:56 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ozwhdr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ozwhdr)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBldditz!</h1>
+      <p>Start editingsdisjfisdjfisjf to see some magic happen :)</p>
+    </div>
+  );
+}
